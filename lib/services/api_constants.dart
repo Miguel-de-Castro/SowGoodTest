cf7c090cb6811abc6fd918b2293d1c147d3841cf
David Campos <david.castro001@edu.pucrs.br>
2023-05-07 17:47:32 -0300

    resolved - #22

--- lib/services/api_constants.dart
+++ lib/services/api_constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   static String baseUrl = const String.fromEnvironment('BASE_URL');
   static String patient = '$baseUrl/patients/self';
-  static String doctor = '$baseUrl/doctors/self';
+  static String doctor = '$baseUrl/patients/doctor/self';
   static String registerPatient = '$baseUrl/patients/register';
   static String getDiaryEvents = '$baseUrl/patients/diary/self';
   static String addDiary = '$baseUrl/patients/diary/create';
