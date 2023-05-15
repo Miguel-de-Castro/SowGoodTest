581299e8a36a43b500e4efb222f914362428fdcb
Joao Victor Granzinoli Cosentino <joao.cosentino@hp.com>
2023-05-15 19:00:06 -0300

    Adding first commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,26 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  const MyApp({Key? key}) : super(key: key);
+  
   @override
   Widget build(BuildContext context) {
+    Widget home;
+    if (const String.fromEnvironment('USER_TYPE') == 'PATIENT') {
+      home = const LoginPage(); // Define a página LoginPage como home
+    } else {
+      if(const String.fromEnvironment('USER_TYPE') == null){
+        home =  const Text('Variavel user type nula.');
+      }
+      home = Scaffold(
+        appBar: AppBar(
+          title: const Text('MyApp'),
+        ),
+        body: const Center(
+          child: Text('Hello, world!'),
+        ),
+      );
+    }
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -23,7 +39,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const LoginPage(),
+      home: home, // Utiliza a variável "home" como a página de início
     );
   }
 }
